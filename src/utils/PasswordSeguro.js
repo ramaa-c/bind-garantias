9edc58c8e1ce430ead87c767416b99bfe9a05766
ramaa-c <raramiro.240@gmail.com>
2026-03-16 13:48:25 -0300

    contraseña segura

---
+++ src/utils/PasswordSeguro.js
@@ -0,0 +1,20 @@
+import zxcvbn from "zxcvbn";
+
+const strengthCache = {};
+
+export const getPasswordScore = (password, email = "") => {
+  if (!password) return 0;
+
+  if (strengthCache[password] !== undefined) {
+    return strengthCache[password];
+  }
+
+  const emailPrefix = email.split("@")[0].toLowerCase();
+  const userInputs = email ? [email, emailPrefix] : [];
+  
+  const result = zxcvbn(password, userInputs);
+  
+  strengthCache[password] = result.score;
+  
+  return result.score;
+};
