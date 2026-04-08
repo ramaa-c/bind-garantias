d331f9fd473d568f19237166f48b9e5142c21475
ramaa-c <raramiro.240@gmail.com>
2026-04-08 14:55:03 -0300

    boton reiniciar

--- src/components/ui/BotonVolver/BotonVolver.jsx
+++ src/components/ui/BotonVolver/BotonVolver.jsx
@@ -5,11 +5,12 @@ import styles from "./BotonVolver.module.css";
 export const BotonVolver = ({ 
   onClick, 
   texto = "Volver al paso anterior",
+  icon: Icon = FaAngleLeft,
 }) => {
   return (
     <div className={styles.contenedorBoton}>
       <button type="button" onClick={onClick} className={styles.btnVolver}>
-        <FaAngleLeft size={16} /> {texto}
+        <Icon size={16} /> {texto}
       </button>
     </div>
   );
