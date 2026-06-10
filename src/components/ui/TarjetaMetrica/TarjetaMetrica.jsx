2d41c488e94ae5366694e2d80afc8ac092a0ec5f
ramaa-c <raramiro.240@gmail.com>
2026-06-10 14:13:31 -0300

    cambios visuales en dshboard

--- src/components/ui/TarjetaMetrica/TarjetaMetrica.jsx
+++ src/components/ui/TarjetaMetrica/TarjetaMetrica.jsx
@@ -1,11 +1,12 @@
 import styles from './TarjetaMetrica.module.css';
 
-export const TarjetaMetrica = ({ icon: Icon, label, value }) => {
+export const TarjetaMetrica = ({ icon: Icon, label, value, footer, className, labelClassName, valueClassName }) => {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${className || ""}`}>
       {Icon && <Icon className={styles.icon} />}
-      <p className={styles.label}>{label}</p>
-      <p className={styles.value}>{value}</p>
+      <p className={`${styles.label} ${labelClassName || ""}`}>{label}</p>
+      <p className={`${styles.value} ${valueClassName || ""}`}>{value}</p>
+      {footer}
     </div>
   );
 };
